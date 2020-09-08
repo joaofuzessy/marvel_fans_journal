09f39740dd87c5b355941b55f937e0eb2bab4d97
João Füzessy <contato@joaofuzessy.com>
2020-09-07 22:53:47 -0300

    Route creation

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import comicsReducer from '../features/comics/comicsSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    comics: comicsReducer
   },
 });
